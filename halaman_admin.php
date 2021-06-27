3212914cd8a664f0122896c9a02b6250f218bd3d
inun7 <sultonainunnazib@gmail.com>
2021-06-27 09:48:45 +0700

    tambah konten di dashboard user

--- halaman_admin.php
+++ halaman_admin.php
@@ -69,14 +69,40 @@
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
 						<div class="row">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Pengajuan Toko</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
+                            <div class="container">
+                                <h5>I.	DASAR HUKUM</h5>
+                                <p>  1.Undang-Undang Nomor 17 Tahun 2006 tentang Perubahan Undang-Undang No. 10 Tahun 1995 tentang Kepabeanan. <br>
+                                     2.	Peraturan Pemerintah Republik Indonesia Nomor 55 Tahun 2008 tentang Pengenaan Bea Keluar Terhadap Barang Ekspor. <br>
+                                     3.	Peraturan Menteri Keuangan Nomor No. 145/PMK.04/2007 jo. PMK No. 148/PMK.04/2011 jo. PMK No. 145/PMK.04/2014 tentang Ketentuan Kepabeanan di Bidang Ekspor. <br>
+                                     4.	Peraturan Menteri Keuangan Nomor 214/PMK.04/2008 jo. PMK No. 146/PMK.04/2014 jo. PMK No. 86/PMK.04/2016 tentang Pemungutan Bea Keluar. <br>
+                                     5.	Peraturan Menteri Keuangan Nomor 224/PMK.04/2015 tentang Pengawasan Terhadap Impor atau Ekspor Barang Larangan dan/atau Pembatasan. <br>
+                                     6.	Peraturan Menteri Keuangan Nomor 13/PMK.010/2017 tentang Penetapan Barang Ekspor yang Dikenakan Bea Keluar dan Tarif Bea Keluar. <br>
+                                     7.	Peraturan Direktur Jenderal Bea dan Cukai Nomor PER-32/BC/2014 jo. PER-29/BC/2016 tentang Tata Laksana Kepabeanan di Bidang Ekspor. <br>
+                                     8.	Peraturan Direktur Jenderal Bea dan Cukai Nomor P-41/BC/2008 jo. P-07/BC/2009 jo. PER-18/BC/2012 jo. PER-34/BC/2016 tentang Pemberitahuan Pabean Ekspor.
+                                </p>
+
+                                <h5>II.	SEKILAS TENTANG EKSPOR</h5>
+                                <p> 1.	Ekspor adalah kegiatan mengeluarkan barang dari daerah pabean. <br>
+                                    2.	Barang ekspor adalah barang yang telah diajukan pemberitahuan ekspor barang dan telah mendapatkan nomor pendaftaran. <br>
+                                    3.	Eksportir adalah orang perseorangan atau badan hukum yang melakukan ekspor. <br>
+                                    4.	Pemberitahuan Ekspor Barang (PEB) adalah pemberitahuan pabean yang digunakan untuk memberitahukan ekspor barang dalam bentuk tulisan di atas formulir atau data elektronik. Bentuk dan isi pemberitahuan pabean ekspor ditetapkan dalam Peraturan Direktur Jenderal Bea dan Cukai. <br>
+                                    5.	Nota Pelayanan Ekspor (NPE) adalah nota yang diterbitkan oleh Pejabat Pemeriksa Dokumen, Sistem Komputer Pelayanan, atau Pejabat Pemeriksa barang atas PEB yang disampaikan, untuk melindungi pemasukan barang yang akan diekspor ke Kawasan Pabean dan/atau pemuatannya ke sarana pengangkut.
+                                </p>
+
+                                <h5>III.	BARANG EKSPOR YANG DIKENAKAN BEA KELUAR</h5>
+                                <p> 1.	Bahwa terhadap barang Ekspor dapat dikenakan Bea Keluar. <br>
+                                    2.	Barang ekspor yang dikenakan Bea Keluar, yaitu sebagai berikut: <br>
+                                    &emsp; a.	Kulit dan Kayu; <br>
+                                    &emsp; b.	Biji kakao; <br>
+                                    &emsp; c.	Kelapa sawit, Crude Palm Oil (CPO), dan produk turunannya; <br>
+                                    &emsp; d.	Produk hasil pengolahan mineral logam; dan <br>
+                                    &emsp; e.	Produk mineral logam dengan kriteria tertentu. <br>
+                                    3.	Perhitungan Bea Keluar adalah sebagai berikut: <br>
+                                    &emsp; a.	dalam hal Tarif Bea Keluar ditetapkan berdasarkan persentase dari Harga Ekspor (advalorum), Bea Keluar dihitung berdasarkan rumus sebagai &emsp; &emsp; &emsp;berikut: <br>
+                                    &emsp; &ensp; Tarif Bea Keluar x Jumlah Satuan Barang x Harga Ekspor per Satuan Barang x Nilai Tukar Mata Uang <br>
+                                    &emsp; b.	dalam hal Tarif Bea Keluar ditetapkan secara spesifik, Bea Keluar dihitung berdasarkan rumus sebagai berikut: <br>
+                                    &emsp; &ensp; Tarif Bea Keluar Per Satuan Barang Dalam Satuan Mata Uang Tertentu x Jumlah Satuan Barang x Nilai Tukar Mata Uang
+                                </p>
                             </div>
                         </div>
                     </div>
